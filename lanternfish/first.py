9d39f07f7a2a63c7f39bcf367b48f8ca76a8b354
aldotele <aldo.telese@hotmail.it>
2021-12-13 08:48:38 +0100

    refactoring lanternfish 1st

--- lanternfish/first.py
+++ lanternfish/first.py
@@ -1,13 +1,14 @@
-puzzle = "4,3,4,5,2,1,1,5,5,3,3,1,5,1,4,2,2,3,1,5,1,4,1,2,3,4,1,4,1,5,2,1,1,3,3,5,1,1,1,1,4,5,1,2,1,2,1,1,1,5,3,3,1,1,1,1,2,4,2,1,2,3,2,5,3,5,3,1,5,4,5,4,4,4,1,1,2,1,3,1,1,4,2,1,2,1,2,5,4,2,4,2,2,4,2,2,5,1,2,1,2,1,4,4,4,3,2,1,2,4,3,5,1,1,3,4,2,3,3,5,3,1,4,1,1,1,1,2,3,2,1,1,5,5,1,5,2,1,4,4,4,3,2,2,1,2,1,5,1,4,4,1,1,4,1,4,2,4,3,1,4,1,4,2,1,5,1,1,1,3,2,4,1,1,4,1,4,3,1,5,3,3,3,4,1,1,3,1,3,4,1,4,5,1,4,1,2,2,1,3,3,5,3,2,5,1,1,5,1,5,1,4,4,3,1,5,5,2,2,4,1,1,2,1,2,1,4,3,5,5,2,3,4,1,4,2,4,4,1,4,1,1,4,2,4,1,2,1,1,1,1,1,1,3,1,3,3,1,1,1,1,3,2,3,5,4,2,4,3,1,5,3,1,1,1,2,1,4,4,5,1,5,1,1,1,2,2,4,1,4,5,2,4,5,2,2,2,5,4,4"
-puzzle = "".join(puzzle.split(","))
-puzzle = [int(el) for el in puzzle]
+f = open('test.txt', 'r')
+puzzle = [int(el) for el in "".join(f.readline().split(","))]
+f.close()
+# print(puzzle)
+
 
 for i in range(80):
     new_puzzle = []
     new_end_of_puzzle = []
-    for el in puzzle:
-        n = int(el)
-        if el == 0:
+    for n in puzzle:
+        if n == 0:
             new_puzzle.append(6)
             new_end_of_puzzle.append(8)
         else:
@@ -23,9 +24,8 @@ def lf(puzzle, c):
         return len(puzzle)
     new_puzzle = []
     new_end_of_puzzle = []
-    for el in puzzle:
-        n = int(el)
-        if el == 0:
+    for n in puzzle:
+        if n == 0:
             new_puzzle.append(6)
             new_end_of_puzzle.append(8)
         else:
